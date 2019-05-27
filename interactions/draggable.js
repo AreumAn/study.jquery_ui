e16ccd844b5a9be98bc21069600fe12dfd2ec554
Areum An <areum@venngage.com>
2019-05-26 22:36:33 -0400

    [draggable] start, drag, stop event

--- interactions/draggable.js
+++ interactions/draggable.js
@@ -1,18 +1,30 @@
 $(function() {
+    var $start_counter = $("#event-start"),
+        $drag_counter = $("#event-drag"),
+        $stop_counter = $("#event-stop"),
+        counts = [0,0,0];
+
     $("#draggable").draggable({
-        axis: "y",
-        cursorAt: {top:0}
+        start: function() {
+            counts[0]++;
+            updateConterStatus($start_counter , counts[0]);
+        },
+        drag: function() {
+            counts[1]++;
+            updateConterStatus($drag_counter , counts[1]);
+        },
+        stop: function() {
+            counts[2]++;
+            updateConterStatus($stop_counter , counts[2]);
+        }
     });
-    $("#draggable2").draggable({
-        axis: "x",
-        cursorAt: {left:0}
-    });
-    $("#draggable3").draggable({
-        containment: "#containment-wrapper",
-        cursor: "move"
-    });
-    $("#draggable4").draggable({
-        containment: "parent",
-        cursor: "crosshair"
-    });
+
+    function updateConterStatus( $event_counter, new_count ) {
+        if ( !$event_counter.hasClass("ui-state-hover")) {
+            $event_counter.addClass("ui-state-hover").siblings().removeClass("ui-state-hover");
+        }
+        $("span.count", $event_counter).text(new_count);
+    }
+
+
 });
