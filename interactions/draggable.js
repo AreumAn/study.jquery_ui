67fdf92e7733f1fceb157a81725e1b8a872f5e41
Areum An <areum@venngage.com>
2019-05-26 22:02:45 -0400

    [draggable] Cursor style

--- interactions/draggable.js
+++ interactions/draggable.js
@@ -1,14 +1,18 @@
 $(function() {
     $("#draggable").draggable({
-        axis: "y"
+        axis: "y",
+        cursorAt: {top:0}
     });
     $("#draggable2").draggable({
-        axis: "x"
+        axis: "x",
+        cursorAt: {left:0}
     });
     $("#draggable3").draggable({
         containment: "#containment-wrapper",
+        cursor: "move"
     });
     $("#draggable4").draggable({
         containment: "parent",
+        cursor: "crosshair"
     });
 });
