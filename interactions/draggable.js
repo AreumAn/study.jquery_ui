498472cdff12c8f86ae0dfa2b1735aed135c422b
Areum An <areum@venngage.com>
2019-05-26 23:00:20 -0400

    [draggable] handles

--- interactions/draggable.js
+++ interactions/draggable.js
@@ -1,30 +1,4 @@
 $(function() {
-    var $start_counter = $("#event-start"),
-        $drag_counter = $("#event-drag"),
-        $stop_counter = $("#event-stop"),
-        counts = [0,0,0];
-
-    $("#draggable").draggable({
-        start: function() {
-            counts[0]++;
-            updateConterStatus($start_counter , counts[0]);
-        },
-        drag: function() {
-            counts[1]++;
-            updateConterStatus($drag_counter , counts[1]);
-        },
-        stop: function() {
-            counts[2]++;
-            updateConterStatus($stop_counter , counts[2]);
-        }
-    });
-
-    function updateConterStatus( $event_counter, new_count ) {
-        if ( !$event_counter.hasClass("ui-state-hover")) {
-            $event_counter.addClass("ui-state-hover").siblings().removeClass("ui-state-hover");
-        }
-        $("span.count", $event_counter).text(new_count);
-    }
-
-
+    $("#draggable").draggable( { handle: "p" });
+    $("#draggable2").draggable( { cancel: "p.ui-widget-header" });
 });
